2ef3ea9624c2a8b6ec13b296ce66d88eb402c0d1
MichaelDimmitt <michaelgdimmitt@gmail.com>
2020-09-22 15:43:21 -0400

    add exit codes to error handling test

--- bash/03-error-handling/error_handling.sh
+++ bash/03-error-handling/error_handling.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-main () { 
+main()
   if (( $# == 1 )); then
     echo "Hello, $1"
+    exit 0
   else 
     echo "Usage: error_handling.sh <person>" 
+    exit 1
   fi
-}
-
 # call main with all of the positional arguments
 main "$@"
+
+# call main with all of the positional arguments
