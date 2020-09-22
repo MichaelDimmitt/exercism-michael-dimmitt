3bc62ba489fd1ad1070471f801691693fa8a92f1
MichaelDimmitt <michaelgdimmitt@gmail.com>
2020-09-22 09:41:32 -0400

    bash exercise 3.

---
+++ bash/03-error-handling/error_handling.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+main () { 
+  if [ "$#" -eq 1 ]; then
+    echo "Hello, $1"
+  else 
+    echo "Usage: error_handling.sh <person>" 
+  fi
+}
+
+# call main with all of the positional arguments
+main "$@"
