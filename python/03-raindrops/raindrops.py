ae8bc00a018c2f846a935ec5d69afaa60f9d9c70
MichaelDimmitt <michaelgdimmitt@gmail.com>
2020-09-22 22:19:12 -0400

    simplify solution by changing to for loop

--- python/03-raindrops/raindrops.py
+++ python/03-raindrops/raindrops.py
@@ -1,30 +1,35 @@
-
-def getPrime(number, mod):
-  #  print( number, mod, number % mod == 0 )
-  return ( number % mod == 0 )
-
-def checkRainNoiseForElement(number, prime):
-  if getPrime(number, prime): return rainNoises[prime]
-  else: return ''
-
-# type: input int => [string, string]
-def gatherPrimes(number):
-  return [checkRainNoiseForElement(number, prime) for prime in [3, 5, 7]]
-
-rainNoises = {
+rain_noises = {
   3: "Pling",
   5: "Plang",
   7: "Plong"
 }
 
-def convert(number): # 27
-  if not isinstance(number, int): raise Exception("Error, input must be an integer!"); 
+## simple version: (for loop)
+def convert(number: int): # 27
+  if not isinstance(number, int): 
+    raise Exception("Error, input must be an integer!"); 
 
-  # additional acceptions that can be thrown:
-  # fail all negative numbers
-  # sys.maxint fail any number larger than the biggest int.
-
-  listOfNoises = ''.join(gatherPrimes(number))
+  rain_string = ""
+  for key in rain_noises.keys():
+    if ( number % key == 0 ):
+      rain_string += rain_noises[key] 
   
-  if len(listOfNoises) == 0: return f"{number}"
-  else: return listOfNoises
+  return rain_string if len(rain_string) != 0 else f"{number}"
+
+## complex version: (list comprehension)
+
+# # removed check_rain_noise_for_element for a simplicity improvement.
+# # def check_rain_noise_for_element(number, key):
+# #  return (rain_noises[key] if ( number % key == 0 )
+
+# def gather_rain_noises(number):
+#   return [ (rain_noises[key] if ( number % key == 0 ) else '') for key in rain_noises.keys()]
+
+# def convert(number): # 27
+#   if not isinstance(number, int): 
+#     raise Exception("Error, input must be an integer!"); 
+#   # additional acceptions that can be thrown:
+#   # fail all negative numbers
+#   # sys.maxint fail any number larger than the biggest int.
+#   list_of_noises = ''.join(gather_rain_noises(number))
+#   return list_of_noises if len(list_of_noises) != 0 else f"{number}"
