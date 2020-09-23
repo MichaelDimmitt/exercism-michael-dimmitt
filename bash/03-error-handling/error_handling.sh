44eff1b8c09b5ce391a5288d9d39e29152f71fc1
MichaelDimmitt <michaelgdimmitt@gmail.com>
2020-09-22 22:44:29 -0400

    cleanup

--- bash/03-error-handling/error_handling.sh
+++ bash/03-error-handling/error_handling.sh
@@ -3,12 +3,8 @@ main(){
   if (( $# != 1 )); then
     echo "Usage: error_handling.sh <person>";
     return 1;
-  else 
-    echo "Hello, $1";
-    return 0;
   fi
+  echo "Hello, $1";
 }
 # call main with all of the positional arguments
-main "$@"
-
-# call main with all of the positional arguments
+main "$@";
