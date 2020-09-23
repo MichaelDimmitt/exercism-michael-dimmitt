b65b19f62585be9de53da70faefe9844263308d9
MichaelDimmitt <michaelgdimmitt@gmail.com>
2020-09-22 23:36:42 -0400

    add error handling to raindrops problem.

--- bash/04-raindrops/raindrops.sh
+++ bash/04-raindrops/raindrops.sh
@@ -5,6 +5,15 @@
 # 7 'Plong'
 
 main(){
+  if (( $# != 1 )); then
+    echo "Error: argument required for raindrops.sh";
+    return 1;
+  elif ! [ "$1" -ge 0 ] 2>/dev/null; then
+    # https://unix.stackexchange.com/a/367406/188491
+    echo "Error: number must be provided as an argument for raindrops.sh";
+    return 1;
+  fi
+
   rain_noises="";
   (($1 % 3 == 0)) && rain_noises+="Pling";
   (($1 % 5 == 0)) && rain_noises+="Plang";
