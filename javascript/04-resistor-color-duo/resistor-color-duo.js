4f9df0104d82b143ce9ca262ee5545bec8b2f5b4
MichaelDimmitt <michaelgdimmitt@gmail.com>
2020-09-25 13:01:03 -0400

    ignore bad input going forward as well as backward

--- javascript/04-resistor-color-duo/resistor-color-duo.js
+++ javascript/04-resistor-color-duo/resistor-color-duo.js
@@ -3,7 +3,6 @@
 // convenience to get you started writing code faster.
 //
 
-
 const colorHashMap = {
   black: 0,
   brown: 1,
@@ -17,7 +16,41 @@ const colorHashMap = {
   white: 9
 }
 
-export const decodedValue = ([code1, code2]) => 
-  (colorHashMap[code1] * 10) + colorHashMap[code2]
-;
+const colorHashMapPlus1 = {
+  black: 1,
+  brown: 2,
+  red: 3,
+  orange: 4,
+  yellow: 5,
+  green: 6,
+  blue: 7,
+  violet: 8,
+  grey: 9,
+  white: 10
+}
 
+export const decodedValue = ([code1, code2, ...rest]) => {
+  try {
+    const recursiveResult = handleErrantInput(code1, code2, rest)
+    if(recursiveResult === 'no recursion, continue'){
+      return (colorHashMap[code1] * 10) + colorHashMap[code2]
+    }
+    else { 
+      return recursiveResult 
+    }
+  }catch(error){
+    console.log({error})
+    throw error
+  }
+};
+
+const handleErrantInput = (code1, code2, rest) => {
+  if( !(colorHashMapPlus1[code1] && colorHashMapPlus1[code2]) ){
+    if( rest.length === 0 ){ throw 'codes provided are not valid!' }
+    else if ( colorHashMapPlus1[code1] ) { return decodedValue([code1, ...rest]) }
+    else if ( colorHashMapPlus1[code2] ) { return decodedValue([code2, ...rest])}
+    else if(rest.length === 1){ throw 'codes provided are not valid!' }
+    else { return decodedValue([...rest]) }
+  }
+  return 'no recursion, continue';
+}
