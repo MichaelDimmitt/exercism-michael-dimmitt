1bffac637995f1607de5d5699fa50a614ad92750
MichaelDimmitt <michaelgdimmitt@gmail.com>
2020-11-07 22:38:42 -0500

    update variable names for gigasecond javascript exercise

--- javascript/05-gigasecond/gigasecond.js
+++ javascript/05-gigasecond/gigasecond.js
@@ -1,7 +1,7 @@
-const SECOND = 1000;
-// const BILLION = 10e8 // Math.pow(10,9)
-
-const ONE_GIGA_SECOND = (1e9 * SECOND)
+const ONE_SECOND_IN_MILLISECONDS = 1000;
+const GIGASECOND = 1E9;
+const GIGASECOND_IN_MILLISECONDS = GIGASECOND * ONE_SECOND_IN_MILLISECONDS
 
 export const gigasecond = (inputDate) => 
-  new Date(inputDate.getTime() + ONE_GIGA_SECOND );
+  new Date(inputDate.getTime() + GIGASECOND_IN_MILLISECONDS );
+;
