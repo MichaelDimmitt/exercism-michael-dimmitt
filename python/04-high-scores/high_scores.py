294fb94abaf7430fe23305728d72568a8d2e45a0
MichaelDimmitt <michaelgdimmitt@gmail.com>
2020-11-09 17:21:29 -0500

    rename highscores.py

--- python/04-high-scores/high_scores.py
+++ python/04-high-scores/high_scores.py
@@ -2,12 +2,11 @@ import bisect
 
 def latest(scores):
     # return scores.pop()
-    return scores[len(scores)-1]
+    return scores[-1]
 
 # Input lists are not sorted so best conceivable runtime is O(n)
 def personal_best(scores):
     return max(scores)
 
 def personal_top_three(scores):
-    scores.sort(reverse=True)
-    return scores[:3]
+    return sorted(scores, reverse=True)[:3]
--- python/tips.md
+++ python/tips.md
@@ -3,3 +3,7 @@ https://www.programiz.com/python-programming/string-interpolation
 Python 3.6 added new string interpolation method called f
 example: 
     f"One for {name}, one for me."
+
+Helpful notes for python lists:
+https://realpython.com/python-reduce-function/#exploring-functional-programming-in-python
+https://realpython.com/introduction-to-python-generators/
