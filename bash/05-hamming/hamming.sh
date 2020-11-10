346335ed3c53be6a637d678521b17e3dce5ab3af
MichaelDimmitt <michaelgdimmitt@gmail.com>
2020-11-09 20:32:26 -0500

    solving-hamming-the-wrong-way

---
+++ bash/05-hamming/hamming.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+main () {
+  [ $# -lt 2 ] && echo "Usage: hamming.sh <string1> <string2>" && exit 1
+  ! [ ${#1} -eq ${#2} ] && echo "left and right strands must be of equal length" && exit 1
+  
+  hammingDistance=$(cmp -bl <(echo -n "$1") <(echo -n "$2") | wc -l )
+  echo $hammingDistance
+}
+
+# call main with all of the positional arguments
+ main "$@"
